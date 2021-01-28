01dea9d4691bfe409ef2594d615c36abcfda002d
YaYeKlinch <klisandrij@gmail.com>
2021-01-28 21:45:03 +0200

    adman added

---
+++ src/main/java/com/examle/springProject/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.examle.springProject.controller;
+
+import com.examle.springProject.domain.User;
+import com.examle.springProject.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model, User user){
+        model.addAttribute("users" , userRepo.findAll());
+        return "userList";
+    }
+    @GetMapping("/{user}")
+    public String userEditForm( @PathVariable("user") User user, Model model){
+        model.addAttribute("user" , user);
+        return "userEdit";
+    }
+    @PostMapping("")
+    public String userSave(User user) {
+
+        userRepo.save(user);
+
+        return "redirect:/user";
+    }
+}
